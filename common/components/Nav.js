63bf82acf900036ddf7cd97e5f8ea6be4653c500
Tirta Nugraha <tirtanugraha@Tirtas-MacBook.local>
2017-10-10 18:12:31 +0700

    fix link

--- common/components/Nav.js
+++ common/components/Nav.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import React from 'react'
-import IndexLink from 'react-router/lib/IndexLink'
-import Link from 'react-router/lib/Link'
+import { IndexLink } from 'react-router'
+import { Link } from 'react-router'
 import { StyleSheet, css } from 'aphrodite'
 
 class Nav extends React.Component {
@@ -13,7 +13,7 @@ class Nav extends React.Component {
         </IndexLink>
         <Link to='/posts' className={css(styles.link)} activeClassName={css(styles.link, styles.activeLink)}> Example Feed
         </Link>
-        <a href='https://gitlab.com/nugrata/universal-react.git' className={css(styles.link)} target='_blank'>Gitlab</a>
+        <a href='https://github.com/tagraha/react-universal' className={css(styles.link)} target='_blank'>github</a>
         <a href='https://twitter.com/tagraha' className={css(styles.link)} target='_blank'>Twitter</a>
       </div>
     )
